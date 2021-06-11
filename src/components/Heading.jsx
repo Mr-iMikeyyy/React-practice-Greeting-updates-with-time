f62ba91cb175faaa556c30488476909e8f928792
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-10 22:19:19 -0400

    Changed style a bit

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,31 +1,29 @@
 import React from "react";
 
-const d = new Date();
-const currentHour = d.getHours();
-console.log(currentHour);
-let greeting;
-const customColor = {
-  color: ""
-};
-
-if (currentHour <= 11) {
-  greeting = "Good Morning!";
-  customColor.color = "red";
-} else if (currentHour < 18) {
-  greeting = "Good Afternoon!";
-  customColor.color = "green";
-} else {
-  greeting = "Good Evening!";
-  customColor.color = "blue";
-}
-
 function Heading() {
+  const d = new Date();
+  const currentHour = d.getHours();
+  console.log(currentHour);
+  let greeting;
+  const customColor = {
+    color: ""
+  };
+
+  if (currentHour <= 11) {
+    greeting = "Good Morning!";
+    customColor.color = "red";
+  } else if (currentHour < 18) {
+    greeting = "Good Afternoon!";
+    customColor.color = "green";
+  } else {
+    greeting = "Good Evening!";
+    customColor.color = "blue";
+  }
+
   return (
-    <div>
-      <h1 className="heading" style={customColor}>
-        {greeting}
-      </h1>
-    </div>
+    <h1 className="heading" style={customColor}>
+      {greeting}
+    </h1>
   );
 }
 
